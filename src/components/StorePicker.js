bccb18fe1dfc4f751338940a1daff0fa22ea6b7a
hamdle <contact@ericmarty.co>
2018-08-20 22:47:52 -0700

    Bind custom component method using constructor - video 11

--- src/components/StorePicker.js
+++ src/components/StorePicker.js
@@ -2,12 +2,33 @@ import React from 'react';
 import { getFunName } from "../helpers";
 
 class StorePicker extends React.Component {
+
+    myInput = React.createRef();
+
+    constructor() {
+        super();
+        this.goToStore = this.goToStore.bind(this);
+    }
+
+    goToStore(event) {
+        // Stop form from submitting
+        event.preventDefault();
+        // Get input data
+        console.log(this);
+        // Redirect to store/data
+    }
+
     render() {
         return (
-            <form className="store-selector">
+            <form className="store-selector" onSubmit={this.goToStore}>
                 { /* comment */ }
                 <h2>Please Enter A Store</h2>
-                <input type="text" required placeholder="Store Name" defaultValue={getFunName()} />
+                <input 
+                    type="text" 
+                    ref={this.myInput}
+                    required placeholder="Store Name" 
+                    defaultValue={getFunName()} 
+                />
                 <button type="submit">Visit Store ></button>
             </form>
         )
