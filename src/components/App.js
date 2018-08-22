e7179f2dee931b7589bd573fe6056e384e721c3d
hamdle <contact@ericmarty.co>
2018-08-22 15:43:16 -0700

    Display order state with JSX - video 15

--- src/components/App.js
+++ src/components/App.js
@@ -3,6 +3,7 @@ import Header from "./Header";
 import Order from "./Order";
 import Inventory from "./Inventory";
 import sampleFishes from '../sample-fishes';
+import Fish from "./Fish";
 
 class App extends React.Component {
 
@@ -36,6 +37,12 @@ class App extends React.Component {
             <div className="catch-of-the-day">
                 <div className="menu">
                     <Header tagline="Fresh Seafood Market" />
+                    <ul className="fishes">
+                        { /* The key property on <Fish> is a react special property */ }
+                        {Object.keys(this.state.fishes).map(key => (
+                            <Fish key={key} details={this.state.fishes[key]} />
+                        ))}
+                    </ul>
                 </div>
                 <Order />
                 <Inventory addFish={this.addFish} loadSampleFishes={this.loadSampleFishes} />
